627787c42a7665f6a530abd2efd09c482d2ed11b
peyton <peyton@peytonmccullough.com>
2013-10-14 19:09:13 -0400

    Document subarray assignment.

--- R/subarray.R
+++ R/subarray.R
@@ -10,18 +10,23 @@
 #' Subset an array based on names
 #' 
 #' \code{subarray} subsets an array based on dimension names, providing
-#' a nice interface.
+#' a nice interface. It can be used to obtain the subset, or to assign
+#' to the subset.
 #' 
 #' @param x the array to subset
 #' @param ... subsetting instructions in the form of \code{dimname=values to keep}
 #' @param drop whether to drop dimensions whenever possible
-#' @return a subset of \code{x}
+#' @return When not used as part of an assignment, \code{subarray} will return the
+#' proper subset of \code{x}.
+#' @rdname subarray
 subarray <- function(x, ..., drop=FALSE) {
   args <- .get.subset.args(x, ...)
   args$drop <- drop
   do.call(`[`, args)
 }
 
+#' @param value the value to assign to the subset
+#' @rdname subarray
 `subarray<-` <- function(x, ..., value) {
   args <- .get.subset.args(x, ...)
   args$value <- value
